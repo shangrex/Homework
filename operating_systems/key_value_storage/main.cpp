5d95c97b05eaf42cee8aa1c001275a5fd6158282
shangrex <rexhuang881019@gmail.com>
2020-12-09 12:42:57 +0800

    Transfer to map<long long int, string>

--- operating_systems/key_value_storage/main.cpp
+++ operating_systems/key_value_storage/main.cpp
@@ -1,40 +1,27 @@
 #include <bits/stdc++.h>
 #include <time.h>
 using namespace std;
+string cache_l[9999][9999];
+string cache_h[9999][9999]; 
+
 int main(int argc, char *argv[]){
     fstream file_in(argv[1], ios::in);
     string s = argv[1];   
-    string delimiter = ".";
-    string out_file = s.substr(0, s.find(delimiter));
-    // fstream file_out(out_file + ".output", ios::out);
+    string out_file;
+    s = s.substr(s.find_last_of("/")+1);
+    cout << s << endl;
+    out_file = s.substr(0, s.find("."));
+    cout << out_file << endl;
+    fstream file_out(out_file + ".output", ios::out);
     clock_t start, end;
+    unordered_map<long long int, string> umap;
     start = clock();
-    // string cache[9999][9999][9999][9999];
     while(file_in >> s){
         string key, value;
         if(s == "PUT"){
             file_in >> key;
             file_in >> value;
-            string c1 = "", c2 = "", c3 = "", c4 = "";
-            // for(int i = 0; i < key.length(); i++){
-            //     if(i < 4){
-            //         c1 += key[i];
-            //     }
-            //     else if(i < 8){
-            //         c2 += key[i];
-            //     }
-            //     else if(i < 12){
-            //         c3 += key[i];
-            //     }
-            //     else if(i < 16){
-            //         c4 += key[i];
-            //     }
-            // }
-            // if(c1 == "")c1 = "0";
-            // if(c2 == "")c2 = "0";
-            // if(c3 == "")c3 = "0";
-            // if(c4 == "")c4 = "0";
-            // cache[stoi(c1)][stoi(c2)][stoi(c3)][stoi(c4)] = value;
+
         }
         else if(s == "GET"){
             file_in >> key;    
