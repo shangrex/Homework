dc2384a5b2c5d4f602162e95ae51b07d1fc94c02
shangrex <rexhuang881019@gmail.com>
2021-11-13 17:59:13 +0800

    Change the assembly code from compiler to human write

--- computer_architecture/hw1/Binary_Number_with_Alternating_Bits.s
+++ computer_architecture/hw1/Binary_Number_with_Alternating_Bits.s
@@ -1,83 +1,23 @@
-	.file	"Binary_Number_with_Alternating_Bits.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"The answer is True."
-.LC1:
-	.string	"The answer is False."
-.LC2:
-	.string	"The answer is %s\n"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB6:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movl	$10, -28(%rbp)
-	movl	-28(%rbp), %eax
-	andl	$1, %eax
-	testl	%eax, %eax
-	setne	%al
-	movb	%al, -29(%rbp)
-	leaq	.LC0(%rip), %rax
-	movq	%rax, -16(%rbp)
-	leaq	.LC1(%rip), %rax
-	movq	%rax, -8(%rbp)
-	movq	-16(%rbp), %rax
-	movq	%rax, -24(%rbp)
-	jmp	.L2
-.L4:
-	sarl	-28(%rbp)
-	movzbl	-29(%rbp), %eax
-	movl	-28(%rbp), %edx
-	andl	$1, %edx
-	cmpl	%edx, %eax
-	jne	.L3
-	movq	-8(%rbp), %rax
-	movq	%rax, -24(%rbp)
-.L3:
-	movl	-28(%rbp), %eax
-	andl	$1, %eax
-	testl	%eax, %eax
-	setne	%al
-	movb	%al, -29(%rbp)
-.L2:
-	cmpl	$0, -28(%rbp)
-	jne	.L4
-	movq	-24(%rbp), %rax
-	movq	%rax, %rsi
-	leaq	.LC2(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	movl	$0, %eax
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE6:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
+.data
+str1:     .string "The answer is True."
+str2:     .string "The answer is False."
+
+.text
+    addi a1, x0, 10           # input n = 10
+    andi t0, a1, 1            # t = n&1
+loop1:
+    beq a1, x0, return_t      # while( n != 0 )
+    srai a1, a1, 1            #  n = n >> 1
+    andi t1, a1, 1            #  t1 =  (n&1)
+    beq t0, t1, return_f      #  if(t == (n&1)) return false
+    andi t0, a1, 1            #  t = n & 1
+    jal ra, loop1
+return_f:
+    la a0, str2               #  print False
+    jal ra, finish
+return_t:
+    la a0, str1               #  print True
+finish:
+    li a7, 4
+    ecall
+    nop
